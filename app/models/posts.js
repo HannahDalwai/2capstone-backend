596bc1d420190e17bed7baecbc4108e18e6e0526
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-03-17 16:28:05 +0200

    testing

--- app/models/posts.js
+++ app/models/posts.js
@@ -13,6 +13,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  category: {
+    type: String,
+    required: true,
+  },
   img: {
     type: String,
     required: false,
